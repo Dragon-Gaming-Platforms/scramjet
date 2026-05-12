a2976ac8144f1db572916df1863c970c3f4a219c
Dragon-Gaming-Platforms <jaxonkhyer@gmail.com>
2026-05-11 19:44:24 -0500

    Fix deploy workflow

--- staticbuild/config.js
+++ staticbuild/config.js
@@ -1,1 +1,4 @@
-let _CONFIG = 
+let _CONFIG = {
+  wispurl: (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/",
+  bareurl: (location.protocol === "https:" ? "https" : "http") + "://" + location.host + "/bare/"
+};
