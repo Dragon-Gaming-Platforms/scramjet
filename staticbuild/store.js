05a558a91e1b871a6dc8a5e466b5ff39785dbe90
Dragon-Gaming-Platforms <jaxonkhyer@gmail.com>
2026-05-12 19:40:41 -0500

    Fix standalone-frontend.html defaults

--- staticbuild/store.js
+++ staticbuild/store.js
@@ -1,13 +1,26 @@
+const params = new URLSearchParams(location.search);
+let config = {};
+const configParam = params.get("config");
+if (configParam) {
+	try {
+		config = JSON.parse(atob(configParam));
+	} catch (err) {
+		console.warn("Failed to parse config query parameter", err);
+	}
+}
+
 const store = $store(
 	{
-		url: "https://google.com",
+		url: config.targetUrl || "https://google.com",
 		wispurl:
+			config.wispUrl ||
 			_CONFIG?.wispurl ||
 			(location.protocol === "https:" ? "wss" : "ws") +
 				"://" +
 				location.host +
 				"/wisp/",
 		bareurl:
+			config.bareUrl ||
 			_CONFIG?.bareurl ||
 			(location.protocol === "https:" ? "https" : "http") +
 				"://" +
